74f79b4826f0054c510509a448b1cd825120fd0d
edwindijas <edwindijas@gmail.com>
2020-11-14 20:22:08 +0200

    Fix tables

--- database/migrations/2020_11_13_142137_create_order_item_table.php
+++ database/migrations/2020_11_13_142137_create_order_item_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateOrdersTable extends Migration
+class CreateOrderItemTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,11 @@ class CreateOrdersTable extends Migration
      */
     public function up()
     {
-        Schema::create('orders', function (Blueprint $table) {
+        Schema::create('order_item', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_id');
-            $table->string('uuid');
-            $table->enum('state', ['pending', 'completed'])->default(0);
-            $table->string('paymentMethod')->nullable();
-            $table->string('payload');
+            $table->integer('package_id');
+            $table->integer('order_id');
+            $table->integer('quantity');
             $table->timestamps();
         });
     }
@@ -31,6 +29,6 @@ class CreateOrdersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('orders');
+        Schema::dropIfExists('order_item');
     }
 }
